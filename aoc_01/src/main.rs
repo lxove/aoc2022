e398313576d72b50f3401a4fa387819e99ad7fd3
Love Jonsson <love.jonsson@bimobject.com>
2022-12-12 21:24:48 +0100

    feat: not so pretty but working

--- aoc_01/src/main.rs
+++ aoc_01/src/main.rs
@@ -1,5 +1,6 @@
 use std::cmp;
 
+
 fn solve_a(input: &str) -> i32{
    let mut max:i32 = 0;
    let mut current:i32 = 0;
@@ -13,8 +14,27 @@ fn solve_a(input: &str) -> i32{
         current = 0;
     }
    }
+   
    max
 }
+
+fn solve_a_mod(input: &str) -> i32 {
+    let tuple: (i32,i32) = input
+        .lines()
+        .map(|line| {
+            return match line.parse::<i32>() {
+                Ok(s) => (s,s),
+                Err(_) => (-1, -1)
+            };
+        })
+        // fold funkar som js reduce, här passar vi (0,0) som startvärde
+        .fold((0,0),|acc:(i32,i32), current:(i32,i32)| {
+            if current.0 < 0 { (cmp::max(acc.0, acc.1), 0) } else { (acc.0, acc.1 + current.0) }
+        });
+
+    return cmp::max(tuple.0, tuple.1);
+}
+
 
 fn solve_b(input: &str) -> i32 {
    let mut max: [i32; 3] = [0,0,0];
@@ -52,7 +72,13 @@ mod tests{
         
     #[test]
     fn solve_b(){
-    assert_eq!(super::solve_b(include_str!("input.txt")),0)
+        assert_eq!(super::solve_b(include_str!("input.txt")),0)
     }
+    
+    #[test]
+    fn solve_a_mod(){
+    assert_eq!(super::solve_a_mod(include_str!("input.txt")),super::solve_a(include_str!("input.txt")))
+    }
+
 }
 
