7661a6cf9d7d3298293fd78eff666c68294dc98e
Love Jonsson <love.jonsson@bimobject.com>
2022-12-13 16:44:32 +0100

    feat: solve aoc_01 part a

--- aoc_02/src/main.rs
+++ aoc_02/src/main.rs
@@ -1,5 +1,26 @@
-fn solve_a(input: &str) -> i32{
-  42
+use itertools::Itertools;
+use std::collections::HashMap;
+
+fn solve_a(input: &str) -> i32 {
+    let scoreMap = HashMap::from([
+        (("A", "X"),4),
+        (("A", "Y"),8),
+        (("A", "Z"),3),
+        (("B", "X"),1),
+        (("B", "Y"),5),
+        (("B", "Z"),9),
+        (("C", "X"),7),
+        (("C", "Y"),2),
+        (("C", "Z"),6)
+    ]);
+
+
+    input.lines()
+        .map(|line| {
+            let tuple = line.split(" ").collect_tuple::<(&str, &str)>().unwrap();
+            scoreMap.get(&tuple).unwrap()
+        })
+        .fold(0,|acc, current| acc + current)
 }
 
 
@@ -9,17 +30,15 @@ fn solve_b(input: &str) -> i32 {
 
 
 #[cfg(test)]
-mod tests{
+mod tests {
     #[test]
     fn solve_a(){
-            assert_eq!(super::solve_a(include_str!("input.txt")),0)
+            assert_eq!(super::solve_a(include_str!("input.txt")),11666)
         }
         
     #[test]
     fn solve_b(){
         assert_eq!(super::solve_b(include_str!("input.txt")),0)
     }
-  
-
 }
 
